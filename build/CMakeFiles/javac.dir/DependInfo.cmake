f053a8e5e690d05d0d43ea73ce1ba090cd5807f7
Lewis Cheng <leecrix@126.com>
2012-07-30 13:36:34 +0800

    haha

---
+++ build/CMakeFiles/javac.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/lewis/projects/javac-compiler/check-tree.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/check-tree.c.o"
+  "/home/lewis/projects/javac-compiler/hashtab.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/hashtab.c.o"
+  "/home/lewis/projects/javac-compiler/lex.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/lex.c.o"
+  "/home/lewis/projects/javac-compiler/main.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/main.c.o"
+  "/home/lewis/projects/javac-compiler/parser.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/parser.c.o"
+  "/home/lewis/projects/javac-compiler/print-tree.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/print-tree.c.o"
+  "/home/lewis/projects/javac-compiler/slist.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/slist.c.o"
+  "/home/lewis/projects/javac-compiler/xmem.c" "/home/lewis/projects/javac-compiler/build/CMakeFiles/javac.dir/xmem.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/javac.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/javac.dir/main.c.o"
+  "CMakeFiles/javac.dir/hashtab.c.o"
+  "CMakeFiles/javac.dir/lex.c.o"
+  "CMakeFiles/javac.dir/parser.c.o"
+  "CMakeFiles/javac.dir/slist.c.o"
+  "CMakeFiles/javac.dir/xmem.c.o"
+  "CMakeFiles/javac.dir/print-tree.c.o"
+  "CMakeFiles/javac.dir/check-tree.c.o"
+  "javac.pdb"
+  "javac"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/javac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
